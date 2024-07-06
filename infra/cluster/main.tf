d934c2fbf1b4a3d6579fb0be5e388fee52dd9a39
Georgios Tsotsos <tsotsos@gmail.com>
2024-07-06 21:47:56 +0300

    Separates structure on Homepage
    Signed-off-by: Georgios Tsotsos <tsotsos@gmail.com>

--- infra/cluster/main.tf
+++ infra/cluster/main.tf
@@ -20,28 +20,8 @@ provider "proxmox" {
   pm_debug            = false
   pm_parallel         = 8
 }
-
-locals {
-  k3s_virtual_machines={
-    "k3s-master-01" = { target_node = "s01", vmid = 201, ignition = "nfs-ds:iso/micro_os_ignition_k3s-master-01.iso", ip = "10.0.2.21", sockets = 1, cores = 4, memory = 8192, storage = "local-lvm", size = 94, template = "OpenSuSE-microOs", tags = "core,master", desc = "OpenSuSE microOs VM"},
-    "k3s-master-02" = { target_node = "s02", vmid = 202, ignition = "nfs-ds:iso/micro_os_ignition_k3s-master-02.iso", ip = "10.0.2.22", sockets = 1, cores = 4, memory = 8192, storage = "local-lvm", size = 94, template = "OpenSuSE-microOs", tags = "core,master", desc = "OpenSuSE microOs VM"},
-    "k3s-master-03" = { target_node = "s03", vmid = 203, ignition = "nfs-ds:iso/micro_os_ignition_k3s-master-03.iso", ip = "10.0.2.23", sockets = 1, cores = 4, memory = 8192, storage = "local-lvm", size = 94, template = "OpenSuSE-microOs", tags = "core,master", desc = "OpenSuSE microOs VM"},
-    "k3s-master-04" = { target_node = "s02", vmid = 204, ignition = "nfs-ds:iso/micro_os_ignition_k3s-master-04.iso", ip = "10.0.2.24", sockets = 1, cores = 2, memory = 8192, storage = "local-lvm", size = 94, template = "OpenSuSE-microOs", tags = "core,master", desc = "OpenSuSE microOs VM"},
-    "k3s-master-05" = { target_node = "s03", vmid = 205, ignition = "nfs-ds:iso/micro_os_ignition_k3s-master-05.iso", ip = "10.0.2.25", sockets = 1, cores = 2, memory = 8192, storage = "local-lvm", size = 94, template = "OpenSuSE-microOs", tags = "core,master", desc = "OpenSuSE microOs VM"},    
-    "k3s-worker-01" = { target_node = "s01", vmid = 206, ignition = "nfs-ds:iso/micro_os_ignition_k3s-worker-01.iso", ip = "10.0.2.26", sockets = 1, cores = 4, memory = 8192, storage = "local-lvm", size = 94, template = "OpenSuSE-microOs", tags = "core,worker", desc = "OpenSuSE microOs VM"},
-    "k3s-worker-02" = { target_node = "s01", vmid = 207, ignition = "nfs-ds:iso/micro_os_ignition_k3s-worker-02.iso", ip = "10.0.2.27", sockets = 1, cores = 2, memory = 8192, storage = "local-lvm", size = 94, template = "OpenSuSE-microOs", tags = "core,worker", desc = "OpenSuSE microOs VM"},
-    "k3s-worker-03" = { target_node = "s02", vmid = 208, ignition = "nfs-ds:iso/micro_os_ignition_k3s-worker-03.iso", ip = "10.0.2.28", sockets = 1, cores = 4, memory = 8192, storage = "local-lvm", size = 94, template = "OpenSuSE-microOs", tags = "core,worker", desc = "OpenSuSE microOs VM"},
-    "k3s-worker-04" = { target_node = "s02", vmid = 209, ignition = "nfs-ds:iso/micro_os_ignition_k3s-worker-04.iso", ip = "10.0.2.29", sockets = 1, cores = 2, memory = 8192, storage = "local-lvm", size = 94, template = "OpenSuSE-microOs", tags = "core,worker", desc = "OpenSuSE microOs VM"},
-    "k3s-worker-05" = { target_node = "s03", vmid = 210, ignition = "nfs-ds:iso/micro_os_ignition_k3s-worker-05.iso", ip = "10.0.2.30", sockets = 1, cores = 4, memory = 8192, storage = "local-lvm", size = 94, template = "OpenSuSE-microOs", tags = "core,worker", desc = "OpenSuSE microOs VM"},
-    "k3s-worker-06" = { target_node = "s03", vmid = 211, ignition = "nfs-ds:iso/micro_os_ignition_k3s-worker-06.iso", ip = "10.0.2.31", sockets = 1, cores = 2, memory = 8192, storage = "local-lvm", size = 94, template = "OpenSuSE-microOs", tags = "core,worker", desc = "OpenSuSE microOs VM"},
-    "k3s-worker-07" = { target_node = "s03", vmid = 212, ignition = "nfs-ds:iso/micro_os_ignition_k3s-worker-07.iso", ip = "10.0.2.32", sockets = 1, cores = 2, memory = 8192, storage = "local-lvm", size = 94, template = "OpenSuSE-microOs", tags = "core,worker", desc = "OpenSuSE microOs VM"},
-    "k3s-worker-08" = { target_node = "s03", vmid = 213, ignition = "nfs-ds:iso/micro_os_ignition_k3s-worker-08.iso", ip = "10.0.2.33", sockets = 1, cores = 2, memory = 8192, storage = "local-lvm", size = 94, template = "OpenSuSE-microOs", tags = "core,worker", desc = "OpenSuSE microOs VM"},
-    "k3s-worker-09" = { target_node = "s03", vmid = 214, ignition = "nfs-ds:iso/micro_os_ignition_k3s-worker-09.iso", ip = "10.0.2.34", sockets = 1, cores = 2, memory = 8192, storage = "local-lvm", size = 94, template = "OpenSuSE-microOs", tags = "core,worker", desc = "OpenSuSE microOs VM"},
-  }
-}
-
 resource "proxmox_vm_qemu" "k3s" {
-  for_each    = local.k3s_virtual_machines
+  for_each    = var.k3s_virtual_machines
   name        = each.key
   desc        = each.value.desc
   target_node = each.value.target_node
