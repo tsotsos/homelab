351ef4699559a86d82c847007099a4d4d00f9f0a
Georgios Tsotsos <tsotsos@gmail.com>
2024-07-12 19:28:28 +0300

    Separates structure on Homepage
    Signed-off-by: Georgios Tsotsos <tsotsos@gmail.com>

--- cluster/bootstrap.sh
+++ cluster/bootstrap.sh
@@ -1,40 +1,73 @@
-#!/bin/zsh
+#!/bin/bash
 
 total_steps=7
 current_step=1
 
-for command in \
-  "kustomize build --enable-alpha-plugins --enable-helm apps/core/sealed-secrets/ | kubectl apply -f -" \
-  "echo 'create secrets\n'"\
-  "kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/cert-manager.yaml > apps/networking/cert-manager.yaml" \
-  "kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/external-dns.yaml > apps/networking/extrenal-dns.yaml" \
-  "kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/grafana-secret.yaml > apps/monitoring/kube-prometheus/grafana-secret.yaml" \
-  "kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/authentik-secret.yaml > apps/security/authentik/secret.yaml" \
-  "kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/db-authentik-credentials.yaml > apps/databases/pg-clusters/authentik/secret.yaml" \
-  "kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/influxdb-credentials.yaml > apps/databases/influxdb/secret.yaml" \
-  "kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/pgadmin-envs-secret.yaml > apps/databases/pgadmin/pgadmin-envs-secret.yaml" \
-  "kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/pgadmin-secret.yaml > apps/databases/pgadmin/pgadmin-secret.yaml" \
-  "kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/argocd-sso-secret.yaml > apps/core/argocd/argocd-sso-secret.yaml" \
-  "echo '-----\n'"\
-  "kustomize build --enable-alpha-plugins --enable-helm apps/networking/ingress-nginx/ | kubectl apply -f -" \
-  "kustomize build --enable-alpha-plugins --enable-helm apps/networking/cert-manager/ | kubectl apply -f -" \
-  "kustomize build --enable-alpha-plugins --enable-helm apps/networking/extrenal-dns/ | kubectl apply -f -" \
-  "sleep 60" \
-  "kustomize build --enable-alpha-plugins --enable-helm apps/core/argocd/ | kubectl apply -f -" \
-  "sleep 60" \
-  "kustomize build --enable-alpha-plugins --enable-helm argo-apps/ | kubectl apply -f -"; do
-  if [[ $command == sleep* ]]; then
-    duration=${command##sleep }
-    end_time=$((SECONDS + duration)) 
-    while ((SECONDS < end_time)); do
-      remaining_time=$((end_time - SECONDS))
-      print -n "\r[$(($current_step * 100 / $total_steps))%] Sleeping... $remaining_time seconds remaining   "
+create_secrets() {
+  kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/cert-manager.yaml > apps/networking/cert-manager/secret.yaml
+  kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/external-dns.yaml > apps/networking/external-dns/secret.yaml
+  kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/grafana-secret.yaml > apps/monitoring/kube-prometheus/grafana-secret.yaml
+  kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/authentik-secret.yaml > apps/security/authentik/secret.yaml
+  kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/db-authentik-credentials.yaml > apps/databases/pg-clusters/authentik/secret.yaml
+  kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/influxdb-credentials.yaml > apps/databases/influxdb/secret.yaml
+  kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/pgadmin-envs-secret.yaml > apps/databases/pgadmin/pgadmin-envs-secret.yaml
+  kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/pgadmin-secret.yaml > apps/databases/pgadmin/pgadmin-secret.yaml
+  kubeseal --scope cluster-wide --controller-name sealed-secrets --controller-namespace kube-system --format yaml --secret-file ../secrets-un/argocd-sso-secret.yaml > apps/core/argocd/argocd-sso-secret.yaml
+}
+
+show_progress() {
+  if [[ $1 == sleep* ]]; then
+    duration=${1##sleep }
+    for ((remaining_time=duration; remaining_time>0; remaining_time--)); do
+      printf "\r[$current_step/$total_steps] Sleeping... %d seconds remaining   " $remaining_time
       sleep 1
     done
+    echo ""
   else
-    # normal progress bar
-    ((current_step++)) && print -n "\r[$(($current_step * 100 / $total_steps))%] "
-    eval $command
+    echo -ne "\r[$current_step/$total_steps] $1"
+    eval $1
+    echo ""
   fi
-done
-print ""
+  ((current_step++))
+}
+
+if [[ $# -eq 0 ]]; then
+  echo "Usage: $0 [--secrets-only]"
+  exit 1
+fi
+
+if [[ $1 == "--secrets-only" ]]; then
+  create_secrets
+elif [[ $1 == "--no-secrets" ]]; then
+  show_progress "kustomize build --enable-alpha-plugins --enable-helm apps/networking/ingress-nginx/ | kubectl apply -f -"
+  show_progress "kustomize build --enable-alpha-plugins --enable-helm apps/networking/cert-manager/ | kubectl apply -f -"
+  show_progress "kustomize build --enable-alpha-plugins --enable-helm apps/networking/external-dns/ | kubectl apply -f -"
+  show_progress "sleep 60"
+  show_progress "kustomize build --enable-alpha-plugins --enable-helm apps/core/argocd/ | kubectl apply -f -"
+  show_progress "sleep 60"
+  show_progress "kustomize build --enable-alpha-plugins --enable-helm argo-apps/ | kubectl apply -f -"
+  show_progress "kubectl apply -f argo-apps/argo-projects.yaml"
+  show_progress "kubectl apply -f argo-apps/root.yaml"
+  show_progress "rm -rf apps/networking/ingress-nginx/charts"
+  show_progress "rm -rf apps/networking/cert-manager/charts"
+  show_progress "rm -rf apps/networking/external-dns/charts"
+  show_progress "rm -rf apps/core/argocd/charts"
+else
+  show_progress "kustomize build --enable-alpha-plugins --enable-helm apps/core/sealed-secrets/ | kubectl apply -f -"
+  create_secrets
+  show_progress "sleep 60"
+  show_progress "kustomize build --enable-alpha-plugins --enable-helm apps/networking/ingress-nginx/ | kubectl apply -f -"
+  show_progress "kustomize build --enable-alpha-plugins --enable-helm apps/networking/cert-manager/ | kubectl apply -f -"
+  show_progress "kustomize build --enable-alpha-plugins --enable-helm apps/networking/external-dns/ | kubectl apply -f -"
+  show_progress "sleep 60"
+  show_progress "kustomize build --enable-alpha-plugins --enable-helm apps/core/argocd/ | kubectl apply -f -"
+  show_progress "sleep 60"
+  show_progress "kubectl apply -f argo-apps/argo-projects.yaml"
+  show_progress "kubectl apply -f argo-apps/root.yaml"
+  show_progress "rm -rf apps/networking/ingress-nginx/charts"
+  show_progress "rm -rf apps/networking/cert-manager/charts"
+  show_progress "rm -rf apps/networking/external-dns/charts"
+  show_progress "rm -rf apps/core/argocd/charts"
+  show_progress "rm -rf apps/core/sealed-secrets/charts"
+  kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d
+fi
