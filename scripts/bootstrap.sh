d74488411374e599f4e368e701afb0a2485a5b2d
Georgios Tsotsos <tsotsos@gmail.com>
2025-08-08 01:41:57 +0300

    applicationset
    Signed-off-by: Georgios Tsotsos <tsotsos@gmail.com>

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+
+BASEPATH="../clusters/"
+CLUSTER="$1"  
+CLUSTER_PATH="${BASEPATH}${CLUSTER}"
+
+echo "🚀 Installing Cert Manager"
+kustomize build --enable-helm "$CLUSTER_PATH"/cert-manager | kubectl apply -f -
+rm -rf "$CLUSTER_PATH"/cert-manager/charts
+
+echo "🚀 Installing Ingress Nginx"
+kustomize build --enable-helm "$CLUSTER_PATH"/ingress-nginx | kubectl apply -f -
+rm -rf "$CLUSTER_PATH"/ingress-nginx/charts
+
+echo "🚀 Installing ArgoCd"
+kustomize build --enable-helm "$CLUSTER_PATH"/argocd | kubectl apply -f -
+rm -rf "$CLUSTER_PATH"/argocd/charts
+
+echo "⏳ Waiting for Argo CD server to be ready..."
+kubectl rollout status deployment/argocd-server -n argocd
+
+echo "🔑 Initial admin password:"
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
+
+echo "✅ Argo CD is installed and ready."
+
+kubectl apply -f "$CLUSTER_PATH"/management-cluster.yaml
